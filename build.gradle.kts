192fdbf476d04d9ba4907a3f7484c2f56c620a0b
greenblueeyes <greenbluey@proton.me>
2025-08-29 23:06:44 -0500

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {   
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {  
+    // Use JUnit Jupiter for testing. 
+    testImplementation(platform("org.junit:junit-bom:5.13.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // This dependency is used by the application.
+    implementation(libs.guava)
+}
+
+application {   
+    // Define the main class for the application.
+
+    //https://docs.gradle.org/current/userguide/application_plugin.html#ex-configure-the-modular-applications-main-module
+    mainClass = "code.Exercise1Main"
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(24)
+    }
+}
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+}
