cd75314c273c921d8d76c978b033dcae6f117d84
greenblueeyes <greenbluey@proton.me>
2025-08-29 23:07:35 -0500

    correct syntax issues

--- src/main/java/code/exercise1.java
+++ src/main/java/code/exercise1.java
@@ -1,55 +1,30 @@
 package code;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Exercise1 {
-    private List<Integer> values = Arrays.toList(new int[1, 15, 20, 25, 30, 48]);
+    private List<Integer> values = Arrays.asList(1, 15, 20, 25, 30, 48);
 
     public List<Integer> findIndices(int targetSum) {
         int idxLeft = 0;
-        int idxRight = values.length - 1;
+        int idxRight = values.size() - 1;
 
-        int sum = values[idxLeft] + values[idxRight];
+        int sum = values.get(idxLeft) + values.get(idxRight);
 
         while (idxLeft != idxRight) {
             
           if (sum == targetSum) {
-            return Arrays.toList(new int[idxLeft, idxRight]);
+            return Arrays.asList(idxLeft, idxRight);
           } else if (sum > targetSum) {
             idxRight--;
           } else if (sum > targetSum) {
             idxLeft++;
           }
+        }  
 
-          // no match found
-          return Collections.emptyList();
-        }
-    
+        return Collections.emptyList();  
     } 
-
-    public static void main(final String[] args) {
-        int targetSum = 50;
-
-        if (args == null || args.length < 1 0 || args[0] == null) {
-            throw new IllegalArgumentException("invalid args: " + Arrays.toString(args));
-        }
-
-        try {
-          targetSum = Integer.parseInt(args[0]);
-        } catch(final NumberFormatException e) {
-            throw new IllegalArgumentException("invalid args: " + Arrays.toString(args));
-        }
-
-        targetSum = args[0];
-            
-        List<Integer> indices = new Exercise().findSum(targetSum);
-        if (indices.isEmpty()) {
-            System.out.println("no match found");
-        } else {
-            System.out.println("Indices: " + indices.toString());
-        }
-        
-    }
 }
